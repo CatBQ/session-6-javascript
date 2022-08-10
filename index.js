f87dd7be95d862d9d5b457831fd65147685d32b0
cat <cbqueta@gmail.com>
2022-08-10 19:01:19 +0100

    added person to sayHey

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-console.log("Hello everyone")
-function sayHey() {
-    console.log("hey")
+
+//console.log("Hello everyone")
+function sayHey(person) {
+    console.log("hey "+ person)
 }
+
+sayHey("Jack")
+
+function conversation() {
+    sayHey();
+    console.log("how are you?");
+    console.log("goodbye");
+}
+
+conversation()
